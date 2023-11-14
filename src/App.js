fa2be869a5393e40f6a59bddb5458a60bd189b0e
Matthew Foyle <mfoyle93@gmail.com>
2023-11-14 13:50:56 +0100

    fix: merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import {
   DynamicContextProvider,
   DynamicWidget,
 } from "@dynamic-labs/sdk-react-core";
+
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { FlowWalletConnectors } from "@dynamic-labs/flow";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
+import { MagicWalletConnectors } from "@dynamic-labs/magic";
+import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto-evm";
 
 const App = () => (
   <DynamicContextProvider
     settings={{
-      environmentId: "4e598b41-f388-489b-a0b3-d24064b1d1ed",
-      walletConnectors: [EthereumWalletConnectors],
+      environmentId: "f0b977d0-b712-49f1-af89-2a24c47674da",
+      walletConnectors: [
+        EthereumWalletConnectors,
+        AlgorandWalletConnectors,
+        SolanaWalletConnectors,
+        FlowWalletConnectors,
+        StarknetWalletConnectors,
+        CosmosWalletConnectors,
+        MagicWalletConnectors,
+        BloctoEvmWalletConnectors,
+      ],
     }}
   >
     <DynamicWidget />
